ebfdc2a5793b2ba2ee3eea8720062cbef827e46b
SergeyVSwift <vashhenkobs@gmail.com>
2023-05-30 11:56:03 +0200

    sprint_09 fix

---
+++ Posti/Appliccation/Constants.swift
@@ -0,0 +1,9 @@
+import UIKit
+
+//MARK: - Key
+let AcessKey = "3OAZUBSyccZg3_EPIdR_EDjfziLvnCZrEOXO2LQ4xyc"
+let SecretKey = "oSSJeGcgGev9Ou8ODtARWB0j4FULj7iJN9DdhOUqOJk"
+let RedirectURI = "urn:ietf:wg:oauth:2.0:oob"
+
+let AccessScope = "public+read_user+write_likes"
+let DefaultBaseURL = URL(string: "https://api.unsplash.com")!
--- Posti/ImagesList/ImagesListViewController.swift
+++ Posti/ImagesList/ImagesListViewController.swift
@@ -76,7 +76,7 @@ extension ImagesListViewController: UITableViewDelegate {
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         performSegue(withIdentifier: ShowSingleImageSegueIdentifier, sender: indexPath)
     }
-
+    
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         guard let image = UIImage(named: photosName[indexPath.row]) else {
             return 0
