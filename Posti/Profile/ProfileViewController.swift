a2f12e4974bd074f7b89cacea25072567553f993
SergeyVSwift <vashhenkobs@gmail.com>
2023-05-30 11:56:03 +0200

    Profile code added

--- Posti/Profile/ProfileViewController.swift
+++ Posti/Profile/ProfileViewController.swift
@@ -2,14 +2,87 @@ import UIKit
 
 final class ProfileViewController: UIViewController {
     
-    @IBOutlet private var avatarImageView: UIImageView!
-    @IBOutlet private var nameLabel: UILabel!
-    @IBOutlet private var loginNameLabel: UILabel!
-    @IBOutlet private var descriptionLabel: UILabel!
+    // MARK: - Properties
+    private let imageView: UIImageView = {
+        let avatar = UIImage(named: "avatar")
+        let imageView = UIImageView(image: avatar)
+        imageView.tintColor = .gray
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
     
-    @IBOutlet private var logoutButton: UIButton!
+    private let nameLabel: UILabel = {
+        let nameLabel = UILabel()
+        nameLabel.text = "Екатерина Новикова"
+        nameLabel.textColor = .white
+        nameLabel.font = nameLabel.font.withSize(23)
+        nameLabel.translatesAutoresizingMaskIntoConstraints = false
+        return nameLabel
+    }()
     
-    @IBAction private func didTapLogoutButton() {
+    private let loginLabel: UILabel = {
+        let loginLabel = UILabel()
+        loginLabel.text = "@ekaterina_nov"
+        loginLabel.textColor = .gray
+        loginLabel.font = loginLabel.font.withSize(13)
+        loginLabel.translatesAutoresizingMaskIntoConstraints = false
+        return loginLabel
+    }()
+    
+    private let descriptionLabel: UILabel = {
+        let descriptionLabel = UILabel()
+        descriptionLabel.text = "Hello, world!"
+        descriptionLabel.textColor = .white
+        descriptionLabel.font = descriptionLabel.font.withSize(13)
+        descriptionLabel.translatesAutoresizingMaskIntoConstraints = false
+        return descriptionLabel
+    }()
+    
+    private let logoutButton: UIButton = {
+        let logoutButton = UIButton()
+        logoutButton.setImage(UIImage(named: "logoutbutton"), for: .normal)
+        logoutButton.tintColor = .red
+        logoutButton.translatesAutoresizingMaskIntoConstraints = false
+        return logoutButton
+    }()
+    
+    // MARK: - Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupActions()
+        setupConstraints()
     }
     
+    // MARK: - private func system
+    private func setupConstraints() {
+        view.addSubview(imageView)
+        imageView.widthAnchor.constraint(equalToConstant: 70).isActive = true
+        imageView.heightAnchor.constraint(equalToConstant: 70).isActive = true
+        imageView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20).isActive = true
+        imageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20).isActive = true
+        
+        view.addSubview(nameLabel)
+        nameLabel.leadingAnchor.constraint(equalTo: imageView.leadingAnchor).isActive = true
+        nameLabel.topAnchor.constraint(equalTo: imageView.bottomAnchor, constant: 8).isActive = true
+        
+        view.addSubview(loginLabel)
+        loginLabel.leadingAnchor.constraint(equalTo: nameLabel.leadingAnchor).isActive = true
+        loginLabel.topAnchor.constraint(equalTo: nameLabel.bottomAnchor, constant: 8).isActive = true
+        
+        view.addSubview(descriptionLabel)
+        descriptionLabel.leadingAnchor.constraint(equalTo: loginLabel.leadingAnchor).isActive = true
+        descriptionLabel.topAnchor.constraint(equalTo: loginLabel.bottomAnchor, constant: 8).isActive = true
+        
+        view.addSubview(logoutButton)
+        logoutButton.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -20).isActive = true
+        logoutButton.centerYAnchor.constraint(equalTo: imageView.centerYAnchor).isActive = true
+    }
+    // MARK: - Logout Button
+    private func setupActions() {
+        logoutButton.addTarget(self, action: #selector(logoutButtonTapped), for: .touchUpInside)
+    }
+    
+    @objc private func logoutButtonTapped() {
+        loginLabel.removeFromSuperview()
+    }
 }
