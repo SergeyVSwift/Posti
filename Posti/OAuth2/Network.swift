4278f4f19234bc0362e584a3fcbddd8940a4ce2c
SergeyVSwift <vashhenkobs@gmail.com>
2023-07-22 01:32:49 +0200

    fix 3.0

--- Posti/OAuth2/Network.swift
+++ Posti/OAuth2/Network.swift
@@ -6,39 +6,6 @@ enum NetworkError: Error {
     case urlSessionError
 }
 
-extension URLSession {
-    func data(for request: URLRequest, completion: @escaping (Result<Data, Error>) -> Void) -> URLSessionTask {
-        
-        let fulfillCompletion: (Result<Data, Error>) -> Void = { result in
-            DispatchQueue.main.async {
-                completion(result)
-            }
-        }
-        
-        let task = dataTask(with: request, completionHandler: { data, response, error in
-            if let data = data,
-               let response = response,
-               let statusCode = (response as? HTTPURLResponse)?.statusCode
-            {
-                if 200 ..< 300 ~= statusCode {
-                    fulfillCompletion(.success(data))
-                } else {
-                    fulfillCompletion(.failure(NetworkError.httpStatusCode(statusCode)))
-                }
-            } else if let error = error {
-                fulfillCompletion(.failure(NetworkError.urlRequestError(error)))
-            } else {
-                fulfillCompletion(.failure(NetworkError.urlSessionError))
-            }
-        })
-        
-        task.resume()
-        return task
-    }
-}
-
-
-
 extension URLSession{
     func objectTask<T:Decodable>(for request: URLRequest,
                                  completion: @escaping (Result<T, Error>) -> Void) -> URLSessionTask {
