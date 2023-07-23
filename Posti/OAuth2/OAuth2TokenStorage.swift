f57afea47e06adc7f98c04af5097f9f976468606
SergeyVSwift <vashhenkobs@gmail.com>
2023-07-23 14:13:53 +0200

    ref

--- Posti/OAuth2/OAuth2TokenStorage.swift
+++ Posti/OAuth2/OAuth2TokenStorage.swift
@@ -1,22 +1,3 @@
-/*import Foundation
-
-final class OAuth2TokenStorage {
-    private let tokenKey = "token"
-
-    var token: String? {
-        get {
-            UserDefaults.standard.string(forKey: tokenKey)
-        }
-        set {
-            if let token = newValue {
-                UserDefaults.standard.set(token, forKey: tokenKey)
-            } else {
-                UserDefaults.standard.removeObject(forKey: tokenKey)
-            }
-        }
-    }
-}*/
-
 import Foundation
 import SwiftKeychainWrapper
 
