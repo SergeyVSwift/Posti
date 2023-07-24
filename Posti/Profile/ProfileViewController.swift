00268f1b2cb434b2b6270dd1741a757d1e742f26
SergeyVSwift <vashhenkobs@gmail.com>
2023-07-24 23:55:11 +0200

    12 try

--- Posti/Profile/ProfileViewController.swift
+++ Posti/Profile/ProfileViewController.swift
@@ -1,15 +1,34 @@
 import UIKit
 import Kingfisher
+import WebKit
+
+protocol ProfileViewControllerProtocol: AnyObject {
+    var presenter: ProfilePresenterProtocol? { get set }
+}
 
 final class ProfileViewController: UIViewController {
     
+    var presenter: ProfilePresenterProtocol?
     private let profileImage = UIImageView()
+    //private var logoutButton: UIButton!
     private let surnameLabel = UILabel()
     private let emailLabel = UILabel()
     private let someTextLabel = UILabel()
-    private let exitButton = UIButton()
+    //  private let exitButton = UIButton()
+    private let storageToken = OAuth2TokenStorage()
     private let profileService = ProfileService.shared
+    private let alertPresenter = AlertPresenter()
     private var profileImageServiceObserver: NSObjectProtocol?
+    
+   private lazy var logoutButton: UIButton = {
+        let logoutButton = UIButton.systemButton(
+            with: UIImage(named: "logoutbutton")!,
+            target: self,
+            action: #selector(self.didTapLogoutButton))
+        logoutButton.tintColor = UIColor(named: "YP Red")
+        logoutButton.translatesAutoresizingMaskIntoConstraints = false
+        return logoutButton
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -34,7 +53,7 @@ final class ProfileViewController: UIViewController {
         view.addSubview(surnameLabel)
         view.addSubview(emailLabel)
         view.addSubview(someTextLabel)
-        view.addSubview(exitButton)
+        view.addSubview(logoutButton)
         
         profileImage.image = UIImage(named: "avatar_image")
         surnameLabel.text = "Sergey o_0"
@@ -46,9 +65,8 @@ final class ProfileViewController: UIViewController {
         someTextLabel.text = "another day"
         someTextLabel.font = .systemFont(ofSize: 13)
         someTextLabel.textColor = .ypWhite
-        exitButton.setImage(UIImage(named: "logoutbutton"), for: .normal)
-        exitButton.tintColor = .ypRed
-        
+        logoutButton.setImage(UIImage(named: "logoutbutton"), for: .normal)
+        logoutButton.tintColor = .ypRed
     }
     
     private func makeConstraints() {
@@ -56,7 +74,7 @@ final class ProfileViewController: UIViewController {
         surnameLabel.translatesAutoresizingMaskIntoConstraints = false
         emailLabel.translatesAutoresizingMaskIntoConstraints = false
         someTextLabel.translatesAutoresizingMaskIntoConstraints = false
-        exitButton.translatesAutoresizingMaskIntoConstraints = false
+        logoutButton.translatesAutoresizingMaskIntoConstraints = false
         
         NSLayoutConstraint.activate([
             profileImage.widthAnchor.constraint(equalToConstant: 70),
@@ -69,8 +87,8 @@ final class ProfileViewController: UIViewController {
             emailLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
             someTextLabel.topAnchor.constraint(equalTo: emailLabel.bottomAnchor, constant: 8),
             someTextLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
-            exitButton.centerYAnchor.constraint(equalTo: profileImage.centerYAnchor),
-            exitButton.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -26)
+            logoutButton.centerYAnchor.constraint(equalTo: profileImage.centerYAnchor),
+            logoutButton.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -26),
         ])
     }
     
@@ -88,8 +106,61 @@ final class ProfileViewController: UIViewController {
         cache.clearDiskCache()
         cache.clearMemoryCache()
     }
+    
+    private func setupActions() {
+        logoutButton.addTarget(self, action: #selector(didTapLogoutButton), for: .touchUpInside)
+    }
+    
+    @objc private func didTapLogoutButton() {
+        storageToken.clearToken()
+        WebViewViewController.clean()
+        guard let window = UIApplication.shared.windows.first else { fatalError("Invalid window configuration") }
+        window.rootViewController = SplashViewController()
+    }
+    
+    static func clean() {
+        HTTPCookieStorage.shared.removeCookies(since: Date.distantPast)
+        WKWebsiteDataStore.default().fetchDataRecords(ofTypes: WKWebsiteDataStore.allWebsiteDataTypes()) { records in
+            records.forEach { record in
+                WKWebsiteDataStore.default().removeData(ofTypes: record.dataTypes, for: [record], completionHandler: {})
+            }
+        }
+    }
+    
+    private func switchToSplashViewController() {
+        guard let window = UIApplication.shared.windows.first else {
+            assertionFailure("Invalid Configuration")
+            return
+        }
+        window.rootViewController = SplashViewController()
+    }
+    
+    private func cleanServicesData() {
+        ImagesListService.shared.clean()
+        ProfileService.shared.clean()
+        ProfileImageService.shared.clean()
+    }
+    
+    private func logout() {
+        OAuth2TokenStorage().token = nil
+        ProfileViewController.clean()
+        cleanServicesData()
+        switchToSplashViewController()
+    }
+    
+    private func showAlert() {
+        let alertController = UIAlertController(
+            title: "Пока, пока!",
+            message: "Уверены, что хотите выйти?",
+            preferredStyle: .alert)
+        alertController.addAction(UIAlertAction(title: "Да", style: .default, handler: { [weak self] action in
+            guard let self = self else { return }
+            self.logout()
+        }))
+        alertController.addAction(UIAlertAction(title: "Нет", style: .default, handler: nil))
+        present(alertController, animated: true, completion: nil)
+    }
 }
-
 extension ProfileViewController {
     private func updateProfileDetails(profile: Profile?) {
         guard let profile = profileService.profile else { return }
