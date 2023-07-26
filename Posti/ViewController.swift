97df54ee9d5e325560b5887823862b27b113e801
Sergey <vashhenkobs@gmail.com>
2023-07-26 12:14:50 +0200

    Revert "Sprint 11"

---
+++ Posti/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Posti
+//
+//  Created by Сергей Ващенко on 27.04.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
